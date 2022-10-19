965c8a77ee477864e2b6d7aa5570ab1315d2e1fb
Vitalii Baliuk <10vba10@gmail.com>
2022-10-20 00:03:18 +0300

    9

---
+++ js/task-2.js
@@ -0,0 +1,21 @@
+const ingredients = [
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
+  ];
+  
+  const listOfIngredients = document.querySelector("#ingredients");
+  
+  
+  const list = ingredients.map((ingredient) => {
+    const li = document.createElement("li");
+    li.textContent = ingredient;
+    li.classList.add("item");
+    return li;
+  });
+  
+  listOfIngredients.append(...list);
+  console.log(listOfIngredients);
---
+++ js/task-5.js
@@ -0,0 +1,12 @@
+const inputRef = document.querySelector("#name-input");
+const spanRef = document.querySelector("#name-output"); 
+
+inputRef.addEventListener("input", (event) => {
+    const inputValue = event.target.value;
+
+    if (inputValue) {
+        spanRef.textContent = inputValue;
+    } else {
+        spanRef.textContent = "Anonymous";
+    }
+});
